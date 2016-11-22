eb8963da59ff14afc1976c4eec7e6e4ed391650f
backflipper1991 <sashaulych@gmail.com>
2016-11-22 11:55:35 +0200

    Gif search v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { Http, Response } from '@angular/http';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css']
 })
+
 export class AppComponent {
-  title = 'app works!';
-}
+  title = 'Welcome to GiphySearch';
+  desc = 'Insert some text to search animated gif'
+  link = 'http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=';
+  http: Http;
+  collection = [];
+  constructor(http: Http) {
+    this.http = http;
+  }
+
+  performSearch(searchTerm: HTMLInputElement): void {
+    var apiLink = this.link + searchTerm.value;
+
+    this.http.request(apiLink)
+      .subscribe((res: Response) => {
+        this.collection = res.json().data;
+        console.log(this.collection);
+      });
+  }
+
+};
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+// import './app/rxjs-extensions';
+import 'zone.js';
+import 'reflect-metadata';
 
-import { AppComponent } from './app.component';
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+import { HttpModule }    from '@angular/http';
+
+//add bootstrap
+import { AlertModule } from 'ng2-bootstrap/ng2-bootstrap';
+import {Ng2PaginationModule} from 'ng2-pagination';
+
+import { AppComponent }  from './app.component';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +21,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AlertModule,
+    Ng2PaginationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
